e6c12e9914f8540c11023cd75db615fc0e1f2a2b
Sherwin Price <sherwinprice@hotmail.com>
2018-05-16 17:25:18 -0400

    begin

---
+++ shell/src/shell.cpp
@@ -0,0 +1,15 @@
+#include "shell.hpp"
+
+int main( int argc, char** argv ){
+	
+GLFWwindow * window;
+
+// Initialise GLFW
+if( !glfwInit() )
+{
+	cout <<  "Failed to initialize GLFW\n";
+	return -1;
+}
+   
+	return 0;
+}
---
+++ shell/src/shell.hpp
@@ -0,0 +1,14 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <epoxy/gl.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+using namespace std;
+
+using std::cout;
+using std::endl;
+using std::ios;
+
+#endif
