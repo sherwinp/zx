7ac5c806b8c05eef3d6e2bf1c6075d90d3d1dd8c
Sherwin Price <sherwin.price@hotmmail.com>
2018-05-16 23:56:45 -0400

    load window

--- shell/src/shell.cpp
+++ shell/src/shell.cpp
@@ -3,6 +3,8 @@
 int main( int argc, char** argv ){
 	
 GLFWwindow * window;
+unsigned int WIDTH = 400;
+unsigned int HEIGHT = 400;
 
 // Initialise GLFW
 if( !glfwInit() )
@@ -10,6 +12,31 @@ if( !glfwInit() )
 	cout <<  "Failed to initialize GLFW\n";
 	return -1;
 }
-   
-	return 0;
+
+glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
+
+window = glfwCreateWindow(WIDTH, HEIGHT, "OpenGL Window", nullptr, nullptr); // Windowed
+glfwMakeContextCurrent(window);
+
+    // Game loop
+    while (!glfwWindowShouldClose(window))
+    {
+        // Check and call events
+        glfwPollEvents();
+
+        // Clear the colorbuffer
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+	
+	// render
+       
+        // Swap the buffers
+        glfwSwapBuffers(window);
+    }
+
+    glfwTerminate();
+return 0;
 }
